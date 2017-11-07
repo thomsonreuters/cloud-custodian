a0f1ba2966397b8262eab4bceb454cc74b8500d7
Aakash Sheth <aakash.sheth@tr.com>
2017-11-07 16:00:32 -0600

    Updated replay_flight_data

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -895,7 +895,7 @@ class S3Test(BaseTest):
         self.patch(s3, 'S3_AUGMENT_TABLE', [
             ('get_bucket_policy', 'Policy', None, 'Policy'),
         ])
-        session_factory = self.record_flight_data('test_s3_no_ssl_statement')
+        session_factory = self.replay_flight_data('test_s3_no_ssl_statement')
         bname = "custodian-ssl-test"
         session = session_factory()
         client = session.client('s3')
