6f42104e0611b54e1fdb3cc55bf9ac3bebf571bf
joh08266 <43418865+joh08266@users.noreply.github.com>
2019-02-06 15:14:15 -0600

    Revert "initial commit"

--- c7n/resources/elasticsearch.py
+++ c7n/resources/elasticsearch.py
@@ -46,7 +46,7 @@ class ElasticSearchDomain(QueryResourceManager):
 
     filter_registry = filters
     _generate_arn = _account_id = None
-    retry = staticmethod(get_retry(('Throttled','ThrottlingException',)))
+    retry = staticmethod(get_retry(('Throttled',)))
 
     @property
     def generate_arn(self):
