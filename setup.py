cf20195c6d392bff0466f01b746d5e7a730757cb
jacob-kinzer <40577885+jacob-kinzer@users.noreply.github.com>
2020-05-27 12:14:41 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         "boto3==1.9.249",
         "botocore==1.12.249",
-        "python-dateutil>=2.6,<3.0.0",
+        "python-dateutil==2.6",
         "pyyaml",
         "jsonschema",
         "jsonpatch>=1.21",
